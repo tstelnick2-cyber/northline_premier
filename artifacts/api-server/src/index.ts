54a4ac958b1d3194b804415576ec5bb93216ec07
abb <abb@pop-os.tail0d90d0.ts.net>
2026-08-10 23:07:39 -0700

    Configure API for Vercel Express deployment

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,13 +1,17 @@
 import app from "./app";
 import { logger } from "./lib/logger";
 
+export default app;
+
 const port = Number(process.env["PORT"] ?? 8080);
 
-app.listen(port, (err) => {
-  if (err) {
-    logger.error({ err }, "Error listening on port");
-    process.exit(1);
-  }
+if (process.env.VERCEL !== "1") {
+  app.listen(port, (err) => {
+    if (err) {
+      logger.error({ err }, "Error listening on port");
+      process.exit(1);
+    }
 
-  logger.info({ port }, "Server listening");
-});
+    logger.info({ port }, "Server listening");
+  });
+}
